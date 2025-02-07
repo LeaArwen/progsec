b026379994ce89556f6d6634b06cc486bd937060
Andreas Lilleby Hjulstad <andreas.lilleby.hjulstad@gmail.com>
2025-02-07 14:22:53 +0100

    Fix API path in Axios when running locally

--- frontend/src/services/AxiosInstance.js
+++ frontend/src/services/AxiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// Changes baseURL for API requests based on the environment, uses localhost/api for prod, and localhost:8000/api for dev
+const baseURL = process.env.NODE_ENV === "development" ? "http://localhost:8000/" : "/"
+
 const AxiosInstance = axios.create({
-  baseURL: "/",
+  baseURL: baseURL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
